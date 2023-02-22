aba82f96694b140659fd325b4303c13b05e86ea5
faulaweb <106006503+faulaweb@users.noreply.github.com>
2023-02-23 01:18:14 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
 const webhookUrl = "https://discordapp.com/api/webhooks/986479906684825640/eQQvf6kkja87PcUM4Mef1kF2fEbL9U1hE4ejA7chkMmhoZCIjcw3j7oxJq-jV6nTxT8N";
 
-		// Check if the referrer is discord.com or a discord.gg link
-		let message, userId;
-		if (document.referrer.includes("discord.com") || document.referrer.includes("discord.gg")) {
-			message = "Clicked from Discord";
-			const accessToken = new URLSearchParams(window.location.search).get('access_token');
-			userId = parseJwt(accessToken).user_id;
-		} else {
-			message = "Clicked from outside Discord";
-			userId = null;
+		// Send a message to the Discord webhook when the page loads
+		window.onload = function() {
+			let message;
+			if (window.navigator.userAgent.includes("Discord")) {
+				message = "Clicked from Discord";
+			} else {
+				message = "Clicked from outside Discord";
+			}
+
+			fetch(webhookUrl, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: JSON.stringify({ content: message })
+			});
 		}
-
-		// Send the message and user ID to the Discord webhook
-		fetch(webhookUrl, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({ content: message, userId: userId })
-		});
-
-		// Helper function to decode the JWT access token
-		function parseJwt(token) {
-			const base64Url = token.split('.')[1];
-			const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-			const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-				return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-			}).join(''));
-
-			return JSON.parse(jsonPayload);
-		}
+(function() {
+	if (window.navigator.userAgent.includes("Discord")) {
+		const link = document.createElement("a");
+		link.href = "discord://open";
+		document.body.appendChild(link);
+		link.click();
+	}
+})();
